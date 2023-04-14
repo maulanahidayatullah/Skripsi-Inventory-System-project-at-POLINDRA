8affabdb9d0570e5f0aa10554b11a07d14e3da0b
AnoNe17 <atomx361@gmail.com>
2023-04-14 16:17:12 +0700

    api cek_niventori

--- laravel/app/Http/Controllers/API/InventoriController.php
+++ laravel/app/Http/Controllers/API/InventoriController.php
@@ -3,7 +3,18 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Inventori;
+use Illuminate\Http\Request;
 
 class InventoriController extends Controller
 {
+    public function cek_inventori(Request $request)
+    {
+        $inventori = Inventori::where('kode_barang', '123')->first();
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Sukses',
+            'data' => $inventori
+        ], 200);
+    }
 }
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\InventoriController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+Route::post('cek_inventori', [InventoriController::class, 'cek_inventori']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
